a8fd911f76f9afefc8bf4f724d80c00cfe791cd4
odeta939 <odeta.penikaite@noaignite.com>
2023-09-13 09:03:19 +0200

    remove logs

--- vinoa-web/app/wines/page.tsx
+++ vinoa-web/app/wines/page.tsx
@@ -2,8 +2,6 @@ import getWines from '@/lib/api/queries/sanityQueries';
 
 const WinePage = async () => {
   const wines: Wine[] = await getWines();
-  console.log('wines');
-  console.log(wines);
   return (
     <div>
       <h1>Wine Page</h1>
