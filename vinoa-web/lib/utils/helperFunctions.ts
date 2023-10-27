33d23a73001d93a995848aeecb49a646bd1bb618
Thomas McSherry <54848048+ThomasMcSherry@users.noreply.github.com>
2023-10-27 15:12:49 +0100

    Fixed a bug in helperFunctions where the current month's range was using the wrong times.

--- vinoa-web/lib/utils/helperFunctions.ts
+++ vinoa-web/lib/utils/helperFunctions.ts
@@ -3,18 +3,20 @@ const getCurrentMonthAsRange = () => {
   start.setDate(1);
   let end = new Date(start);
   end.setMonth(end.getMonth() + 1);
-  
+
   let from = start.toISOString();
+  from = from.substring(0, from.indexOf('T'));
   let to = end.toISOString();
+  to = to.substring(0, to.indexOf('T'));
 
   return { from, to };
-}
+};
 
-const getAverageRating= (reviews: Review[]) => {
-  let sum =0
+const getAverageRating = (reviews: Review[]) => {
+  let sum = 0;
   reviews.forEach((review) => {
     sum += review.rating;
   });
   return sum / reviews.length;
-}
-export { getAverageRating , getCurrentMonthAsRange};
+};
+export { getAverageRating, getCurrentMonthAsRange };
