aca6830a358bebdc69c10c24fb387ac6d1768e21
Thomas McSherry <54848048+ThomasMcSherry@users.noreply.github.com>
2023-10-27 10:36:30 +0100

    Linked WineTastingCard to wine page; Made it possible to add a border via props to WineTastingCard.

--- vinoa-web/components/WineTastingCard/WineTastingCard.tsx
+++ vinoa-web/components/WineTastingCard/WineTastingCard.tsx
@@ -1,34 +1,43 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 interface WineTastingProps {
   wine: Wine;
+  border?: boolean;
 }
 export default function WineTastingCard({
-  wine: { name, imageUrl, tag, price, taste, smell, body },
+  wine: { name, slug, imageUrl, tag, price, taste, smell, body },
+  border = false,
 }: WineTastingProps) {
   return (
-    <div className='w-64 h-[390px] xl:w-80 xl:h-[460px] p-4 px-5 bg-neutral-50 rounded-3xl shadow-[4px_4px_4px_rgba(0,0,0,0.45)]'>
-      <div className='w-28 h-48 xl:w-32 xl:h-56 mx-auto relative'>
-        <Image
-          className='rounded-3xl'
-          alt='Image of a wine bottle'
-          src={`${imageUrl}`}
-          layout='fill'
-          objectFit='cover'
-        />
-      </div>
-      <div className='bg-neutral-50 w-16 h-7 rounded-3xl text-center ring-black ring-1 text-black text-sm mt-4 xl:mt-6 py-1'>
-        {tag}
-      </div>
-      <div className='text-black mt-3'>
-        <p className='text-xl xl:text-2xl'>{name}</p>
-        <p className='text-wine-red text-base xl:text-lg'>{price} NOK</p>
-        <div className='grid grid-cols-2 gap-x-10 gap-[2px] xl:text-base text-sm'>
-          <p>Taste</p> <p>{taste}</p>
-          <p>Aroma</p> <p>{smell}</p>
-          <p>Body</p> <p>{body}</p>
+    <Link href={`/wines/${slug}`}>
+      <div
+        className={`m-auto w-80 h-[460px] sm:w-64 sm:h-[390px] xl:w-80 xl:h-[460px] p-4 px-5 mb-1 bg-neutral-50 rounded-3xl shadow-[4px_4px_4px_rgba(0,0,0,0.45)] ${
+          border && 'border-wine-red border-4'
+        }`}
+      >
+        <div className='w-28 h-48 xl:w-32 xl:h-56 mx-auto relative'>
+          <Image
+            className='rounded-3xl'
+            alt='Image of a wine bottle'
+            src={`${imageUrl}`}
+            layout='fill'
+            objectFit='cover'
+          />
+        </div>
+        <div className='bg-neutral-50 w-16 h-7 rounded-3xl text-center ring-black ring-1 text-black text-sm mt-4 xl:mt-6 py-1'>
+          {tag}
+        </div>
+        <div className='text-black mt-3'>
+          <p className='text-xl xl:text-2xl'>{name}</p>
+          <p className='text-wine-red text-base xl:text-lg'>{price} NOK</p>
+          <div className='grid grid-cols-2 gap-x-10 gap-[2px] xl:text-base text-sm'>
+            <p>Taste</p> <p>{taste}</p>
+            <p>Aroma</p> <p>{smell}</p>
+            <p>Body</p> <p>{body}</p>
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 }
